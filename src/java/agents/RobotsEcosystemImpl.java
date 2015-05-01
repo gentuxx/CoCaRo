13f5a0ae8af1c01f98bbc943ad69a83018c4c4b9
Metabolic24 <metabolic.ewilan@hotmail.fr>
2015-05-01 12:03:16 +0200

    Résolution problème constructeur

--- src/java/agents/RobotsEcosystemImpl.java
+++ src/java/agents/RobotsEcosystemImpl.java
@@ -8,7 +8,7 @@ import speadl.agents.RobotsEcosystem;
 public class RobotsEcosystemImpl extends RobotsEcosystem{
 
 	@Override
-	protected Robot make_Robot(String identifier, Color color) {
+	protected Robot make_Robot(final String identifier, final Color color) {
 		Robot robot = new Robot(){
 
 			@Override
