4ab8ad34c53464d5d1065d69758480487d360fac
Metabolic24 <metabolic.ewilan@hotmail.fr>
2015-05-01 12:00:09 +0200

    Résolution d'un bug d'import

--- src/java/environment/EnvironmentImpl.java
+++ src/java/environment/EnvironmentImpl.java
@@ -1,11 +1,20 @@
 package java.environment;
 
-import java.agents.test.Environment;
+import speadl.agents.RobotsEcosystem;
+import speadl.environment.Environment;
+import speadl.environment.Grid;
 
 public class EnvironmentImpl extends Environment{
 
+
 	@Override
-	protected IBoxGenerator make_createBox() {
+	protected Grid make_grid() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected RobotsEcosystem make_robotEcosystem() {
 		// TODO Auto-generated method stub
 		return null;
 	}
